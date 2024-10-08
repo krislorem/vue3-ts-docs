57d5d36ea072034d92d7c28388728f96751ee782
krislorem <2145619745@qq.com>
2024-10-08 10:05:51 +0800

    init

--- .vitepress/dist/assets/faq_index.md.Cd1fHbFt.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./chunks/framework.D84hx5uF.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"faq/index.md","filePath":"faq/index.md"}'),n={name:"faq/index.md"};function r(o,s,c,d,i,f){return a(),t("div")}const m=e(n,[["render",r]]);export{_ as __pageData,m as default};
